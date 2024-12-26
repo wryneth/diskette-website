4c16d5229c6f68bd2c8b9c9062e7356ddcba5456
Wren <155106259+DisketteKitchen@users.noreply.github.com>
2024-12-25 20:08:48 -0700

    Fix Favicon?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,12 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+
     }
+
   },
 
   modules: [
