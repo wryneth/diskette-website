a7b034d23d2a646fad42a153394a7f0dbb48af4c
Wren <155106259+DisketteKitchen@users.noreply.github.com>
2024-12-27 13:54:36 -0700

    Update VidFeed.vue

--- components/VidFeed.vue
+++ components/VidFeed.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <LatestYouTubeVideos :channelId="'UCWCgjck7Tr8avF--km63lbw'" :apiKey="process.env.TUBE" />
+    <LatestYouTubeVideos :channelId="'UCWCgjck7Tr8avF--km63lbw'" :apiKey="apiKey" />
   </div>
 </template>
 
 <script>
-
 import LatestYouTubeVideos from './components/RecentVideos.vue';
 
 export default {
   components: {
     LatestYouTubeVideos
+  },
+  data() {
+    return {
+      apiKey: process.env.VUE_APP_TUBE // Access API key from environment variable
+    };
   }
 };
 </script>
