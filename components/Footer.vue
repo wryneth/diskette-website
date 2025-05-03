7255ba06129b87437b92ed87d5777832e8e67182
Wren <155106259+DisketteKitchen@users.noreply.github.com>
2025-05-03 12:03:38 -0600

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,6 +6,6 @@ import {Card} from '@/components/ui/card'
 <template>
 <Card class="m-5">
   <a href="#" class="text-center">Back to top</a>
-  <USeparator label="©2025 Diskette Media" />
+  <USeparator label="Designed and built with ❤️ in Bozeman, Montana ©2025 Diskette Media" />
 </Card>
 </template>w
