82c7e26fea2ce8cf0afa1c65ed05b294c6d5ae8b
Wren <155106259+DisketteKitchen@users.noreply.github.com>
2025-05-12 00:21:33 -0600

    Fixed Bluesky link

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="ms-5 flex gap-2">
       <a href="https://youtube.com/@diskettekitchen"><Icon class="w-5 h-6" icon="mdi:youtube" /></a>
-      <a href="https://"><Icon class="w-5 h-6" icon="ri:bluesky-fill" /></a>
+      <a href="https://bsky.app/profile/diskettekitchen.com"><Icon class="w-5 h-6" icon="ri:bluesky-fill" /></a>
       <a href="https://instagram.com/diskettekitchen"><Icon class="w-5 h-6" icon="mdi:instagram" /></a>
       <a href="https://printables.com/@diskette"><Icon class="w-5 h-6" icon="simple-icons:printables" /></a>
       <a href="https://github.com/DisketteKitchen"><Icon class="w-5 h-6" icon="mdi:github" /></a>
