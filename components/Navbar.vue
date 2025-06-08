d86064e48075715cfc3d685d32fefc5a01891c1f
Wren <155106259+DisketteKitchen@users.noreply.github.com>
2025-06-07 21:52:06 -0600

    Remove placeholder collection

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="fixed top-5 left-0 right-0 z-3 mx-auto w-full max-w-5xl p-5 ">
-    <nav
-        class="flex h-16 items-center justify-between rounded-full border border-white/20 bg-white/50 px-6 shadow-sm backdrop-blur-md border-black-40 dark:bg-black/0">
+    <nav class="flex h-16 items-center justify-between rounded-full border border-white/20 bg-white/50 px-6 shadow-sm backdrop-blur-md border-black-40 dark:bg-black/0">
       <!-- Logo -->
       <div class=" flex items-center">
           <img src="/images/eyes.svg" alt="Weave" class="w-14 h-14"/>
@@ -18,7 +17,8 @@
             :class="{
             'bg-white/20 text-gray-900 dark:bg-slate-800/20 dark:text-white': item.current,
             'text-gray-700 dark:text-gray-300': !item.current
-          }">{{ item.name }}</a>
+          }">{{ item.name }}
+        </a>
 
         <SocialLinks />
 
@@ -87,7 +87,6 @@ const navigationItems = [
   {name: 'Videos', href: '#vidfeed', current: false},
   {name: 'About', href: '#about', current: false},
   {name: 'Projects', href: '#projects', current: false},
-  {name: 'Collection', href: '#collect', current: false},
   {name: 'Contact', href: '#contact', current: false},
   {name: 'Store', href: 'https://store.diskettekitchen.com', current: false},
   {name: 'Extras', href: 'https://extras.diskettekitchen.com', current: false},
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,6 @@
   <ProjectCard Image="case" ProjectTitle="Diskette://Case" videoLink="https://youtu.be/kKrAZ_lCJe4?si=PDwqkvX83yAjxRs9Y" downloadLink="https://www.printables.com/model/1073452-diskette-case-modular-open-source-nas-chassis" ProjectDescription="A fully 3d printed, modular computer case optimized to be a NAS chassis. Accommodates a fully passively cooled ITX board, eight 2.5 in drives, and a full ATX sized PSU." />
   <ProjectCard ProjectTitle="Disassembled iPod Shuffle" videoLink="https://youtu.be/pcYXZlmIuOg?si=joGwtisLZCiHDdxt" downloadLink="https://www.printables.com/model/981437-ipod-shuffle-4th-gen-dismantled-art" ProjectDescription="Exploded view of an iPod Shuffle 4th generation, showing all the internal parts, & specifications in a shadow box."/>
   <ProjectCard Image="book" ProjectTitle="Custom MacBook Logo" videoLink="https://youtu.be/MmODnbPsxqY?si=vHDM1XwtUzUPnBgH" downloadLink="https://www.printables.com/model/981437-ipod-shuffle-4th-gen-dismantled-art" ProjectDescription="With the help of some overhead projector transparencies, this 2010 MacBook Pro now has a custom retro Apple logo."/>
-  <CollectLanding
   <Contact />
   <Footer />
   </template>
